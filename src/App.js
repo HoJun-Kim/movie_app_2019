5d65f5439cc7ce41ffe87593a8a2de9a5003c312
Hojun_Kim <ghwns5338@gmail.com>
2019-12-09 21:25:48 +0900

    setState와 render의 관계
    현재 스테이트를 주는 함수 current

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ class App extends React.Component{
   state = {
     count : 0
   };
-  add = () => {console.log("add")};
-  minus = () => {console.log("minus")};
+  
+  add = () => {this.setState({count : this.state.count + 1})};
+  //state를 set할때 react에서 외부의 상태에 의존하지 않는 가장 좋은 방법 아래
+  //리액트는 현재 스테이트를 준다. 위 보다 좋은 소스.
+  minus = () => {this.setState(current => ({count : current.count - 1}))};
   render(){
+
   return( 
     <div>
       <h1>The number is {this.state.count}</h1>
