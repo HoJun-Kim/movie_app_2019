8dfaa05ce51603f06a556d90316aafc4236ab232
Hojun_Kim <ghwns5338@gmail.com>
2019-12-11 21:39:07 +0900

    섹션나누기, css클래스 작성

--- src/Movie.js
+++ src/Movie.js
@@ -3,7 +3,14 @@ import PropTypes from 'prop-types';
 
 //state가 필요없는 이 movie컴포넌트는 class로 만들 필요가없겠지?
 function Movie({id,year,title,summary,poster}){
-    return <h5>{title}</h5>
+    return <div class="movie">
+            <img src={poster} alt={title} title={title} />
+        <div class="movie_data">
+        <h3 class="movie_title">{title}</h3>
+        <h5 class="movie_year">{year}</h5>
+        <p class="movie_summary">{summary}</p>
+        </div>
+    </div>
 }
 
 Movie.propTypes = {
