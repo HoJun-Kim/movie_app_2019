4ab0506a86e29cf529013f9f3a81c1592cf23334
Hojun_Kim <ghwns5338@gmail.com>
2019-12-11 21:59:50 +0900

    장르추가, className,map함수 두번째인자

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios'; //싱글쿼츠 더블쿼츠 결과가 다른데..? 이게 최신인가..?
 import Movie from './Movie'; // 동일 패키지라는뜻
+import './App.css';
 
 //class component. state에 관하여
 class App extends React.Component{
@@ -20,15 +21,21 @@ class App extends React.Component{
     this.getMovies();
   };
 
-  //axios는 느릴 수 있다. 그럴때는 결과를 기다리게 해야지? async 비동기 선언
-  //async componentDidMount(){//마운트+렌더링 다음 호출되는 컴포넌트죠? 
+  //axios는 느릴 수 있다. async 비동기 선언
+  //async componentDidMount(){//마운트+렌더링 다음 호출되는 컴포넌트 
   //  const movies = axios.get("https://yts-proxy.now.sh/list_movies.json");
   //}
 
   render() {  
     const {isLoading, movies} = this.state; //ES6 문법. 늘 this를 적기 귀찮으니까
     return(
-    <div>{isLoading ? "Loading" : movies.map(movie => 
+    <section className="container">
+      {isLoading ? (
+      <div className="loader">
+      <span className = "loader_text">Loading...</span>
+      </div> ) : (
+        <div className="movies">
+          {movies.map(movie => (
                 <Movie 
                   key = {movie.id}
                   id={movie.id} 
@@ -36,9 +43,12 @@ class App extends React.Component{
                   title={movie.title} 
                   summary={movie.summary} 
                   poster={movie.medium_cover_image} 
+                  genres={movie.genres}
                 />
-              )}
+              ))}
         </div>
+        )}
+       </section>
       );
     }
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Movie.css';
 
 //state가 필요없는 이 movie컴포넌트는 class로 만들 필요가없겠지?
-function Movie({id,year,title,summary,poster}){
-    return <div class="movie">
-            <img src={poster} alt={title} title={title} />
-        <div class="movie_data">
-        <h3 class="movie_title">{title}</h3>
-        <h5 class="movie_year">{year}</h5>
-        <p class="movie_summary">{summary}</p>
+function Movie({year,title,summary,poster,genres}){
+    return <div className="movie">
+            <img src={poster} alt={title} title={title} /> 
+        <div className="movie_data">
+        <h3 className="movie_title">{title}</h3>
+        <h5 className="movie_year">{year}</h5>
+        <ul className="genres">
+            {genres.map((genre, index) => (
+                <li key={index} className="genres_genre">
+                    {genre}
+                </li>
+            ))}
+        <p className="movie_summary">{summary}</p>
+                </ul>
         </div>
     </div>
 }
@@ -18,7 +26,8 @@ Movie.propTypes = {
     year  : PropTypes.number.isRequired,
     title : PropTypes.string.isRequired,
     summary : PropTypes.string.isRequired,
-    poster : PropTypes.string.isRequired
+    poster : PropTypes.string.isRequired,
+    genres : PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default Movie; //don't forget
