cb1431042a207e79631e86afaab73f9d2901eb59
Hojun_Kim <ghwns5338@gmail.com>
2019-12-13 22:44:31 +0900

    Updates

---
+++ precache-manifest.1ff27b8092a0f0f83d7a220a3a7e2f2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e66d2c0d4a44794e84923f1fd04ef5e8",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "e1bc56977bfe57f57827",
+    "url": "/movie_app_2019/static/css/main.f6b3acb1.chunk.css"
+  },
+  {
+    "revision": "776b3fde604bcb3ddf35",
+    "url": "/movie_app_2019/static/js/2.a070a377.chunk.js"
+  },
+  {
+    "revision": "e1bc56977bfe57f57827",
+    "url": "/movie_app_2019/static/js/main.163b7ed9.chunk.js"
+  },
+  {
+    "revision": "58e37996626726930b78",
+    "url": "/movie_app_2019/static/js/runtime-main.3cf556df.js"
+  }
+]);
